588755ac23242f660834c2b8f5b60eb13630b72a
Matt Brigida <mdbrigida@gmail.com>
2022-10-06 08:16:53 -0400

    can get all current institutions

--- src/bank_find_api/helpers.py
+++ src/bank_find_api/helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 base_url = "https://banks.data.fdic.gov/api/"
 
@@ -14,6 +15,18 @@ def all_institutions():
     ## do more handling of types here-------
 
     return(institutions)
+
+## working------
+def all_active_institutions():
+    active_institutions = requests.get(base_url + 'institutions', params={'filters':'ACTIVE:1', 'limit':6000}).json()['data']
+
+    active_institutions_df = pd.concat([pd.DataFrame(active_institutions[i]) for i in range(len(active_institutions))], 1)
+    active_institutions_df = active_institutions_df.drop(["score"], 1)
+    active_institutions_df.columns = active_institutions_df.loc["FED_RSSD"]
+    active_institutions_df = active_institutions_df.transpose()
+    return(all_active_institutions)
+
+
 
 
 ### get all branch locations-------
@@ -33,12 +46,14 @@ def all_institutions():
 test_cert = 6560
 test_rssd = 12311
 
-import requests
-
-### working
-hban = requests.get(base_url + 'financials', params={'filters':'REPDTE:20220630', 'limit':20}).json()['data']
+### lets try pulling everything------
+### maybe not possible---stops with an error (Extra data: line 1 column 5 (char 4))
+## maybe loop through list of certs
+## following works, amybe loop through all RSSDIDs and append to dataframe
+## looks like I can get all active banks with an institutions query with ACTIVE:1 filter
+hban = requests.get(base_url + 'financials', params={'filters':'REPDTE:20220630 AND RSSDID:12311', 'limit':10}).json()['data']
 
 hban_df = pd.concat([pd.DataFrame(hban[i]) for i in range(len(hban))], 1)
 hban_df = hban_df.drop(["score"], 1)
-hban_df.columns = hban_df.loc["CERT"]
-hban_df.loc["CERT"]
+hban_df.columns = hban_df.loc["RSSDID"]
+hban_df = hban_df.transpose()
