1e902fa5cb5d731e3450efd671cd66873f6e7514
Matt Brigida <mdbrigida@gmail.com>
2022-10-15 19:52:40 -0400

    looks like you can only get yearly and not by quarter?

--- src/bank_find_api/number_of_banks_by_quarter.py
+++ src/bank_find_api/number_of_banks_by_quarter.py
@@ -3,5 +3,9 @@ import requests
 
 base_url = "https://banks.data.fdic.gov/api/"
 
-def number_by_quarter(start, end):
-    number = requests.get(base_url + 'summary', params={'filters':'ACTIVE:1', 'limit':6000}).json()['data']
+def number_by_year(year):
+    ### looks like only returns yearly data--------
+    number = requests.get(base_url + 'summary', params={'filters':'YEAR:[' + str(year) + ' TO ' + str(year) + ']', 'limit':6000, 'fields':'BANKS'}).json()['data']
+    number_df = pd.concat([pd.DataFrame(number[i]) for i in range(len(number))], 1)
+    number_df = number_df.drop(["score"], 1)
+
