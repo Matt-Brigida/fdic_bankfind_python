93a43ac0334be5b31cef0dacfdc79a8ded273a72
Matt Brigida <mdbrigida@gmail.com>
2023-11-22 11:45:51 -0500

    removed - from git

--- src/bulkcr/get_institutions.py
+++ src/bulkcr/get_institutions.py
@@ -4,18 +4,18 @@ import time
 
 base_url = "https://banks.data.fdic.gov/api/"
 
--### get all institutions that have ever existed-------
--
--def all_institutions():
--    institutions_url = "https://s3-us-gov-west-1.amazonaws.com/cg-2e5c99a6-e282-42bf-9844-35f5430338a5/downloads/institutions.csv"
--
--    institutions = pd.read_csv(institutions_url)
--    institutions.index = institutions["FED_RSSD"]
--    institutions = institutions.drop(["FED_RSSD"], axis=1)
--    institutions["DATEUPDT"] = pd.to_datetime(institutions["DATEUPDT"])
--    ## do more handling of types here-------
--
--    return(institutions)
+### get all institutions that have ever existed-------
+
+def all_institutions():
+    institutions_url = "https://s3-us-gov-west-1.amazonaws.com/cg-2e5c99a6-e282-42bf-9844-35f5430338a5/downloads/institutions.csv"
+
+    institutions = pd.read_csv(institutions_url)
+    institutions.index = institutions["FED_RSSD"]
+    institutions = institutions.drop(["FED_RSSD"], axis=1)
+    institutions["DATEUPDT"] = pd.to_datetime(institutions["DATEUPDT"])
+    ## do more handling of types here-------
+
+    return(institutions)
 
 ## cant seem to pull other run dates, so not clear how to get historical data----
 # t = requests.get(base_url + 'locations', params={'filters':'RUNDATE:20231013'}).json()['data'] ## RUNDATE:20231020 (can we get historical run dates)
